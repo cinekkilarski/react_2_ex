3a42abacc32791f7f63d369f790d4686f7997e02
cinekmazur <cinek.mazur@gmail.com>
2020-04-20 09:02:09 +0200

    Updates

---
+++ precache-manifest.7fb78ec4023312421a740c652bd6285f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a39f2747686aef1eb588",
+    "url": "/react_2_ex/static/css/main.39566c57.chunk.css"
+  },
+  {
+    "revision": "a39f2747686aef1eb588",
+    "url": "/react_2_ex/static/js/main.a39f2747.chunk.js"
+  },
+  {
+    "revision": "48e065d6977ee58f717a",
+    "url": "/react_2_ex/static/js/runtime~main.48e065d6.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/react_2_ex/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/react_2_ex/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/react_2_ex/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/react_2_ex/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "5bac91c9fe82f432a143b73c94572c14",
+    "url": "/react_2_ex/index.html"
+  }
+];
